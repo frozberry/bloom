ef1c62ac07c5ac0c9fb1159df20df0299acc0cdd
frozberry <93860959+frozberry@users.noreply.github.com>
2022-02-17 13:09:06 +0000

    Create api route

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,8 +3,17 @@ import HowItWorks from "../components/landing-page/HowItWorks"
 import WhatsWaterfront from "../components/landing-page/WhatsWaterfront"
 import LandingFooter from "../components/landing-page/LandingFooter"
 import LandingGraphs from "../components/landing-page/LandingGraphs"
+import { useEffect } from "react"
+import axios from "axios"
 
 const LandingPage = () => {
+  useEffect(() => {
+    const foo = async () => {
+      const res: any = await axios.get("/api/users")
+      console.log(res.data.name)
+    }
+    foo()
+  }, [])
   return (
     <>
       <LandingHeader />
