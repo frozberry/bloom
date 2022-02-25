47c0fa9036336057706b1570058806b934b71944
frozberry <93860959+frozberry@users.noreply.github.com>
2022-02-25 17:30:43 +0000

    Create profile routes

---
+++ src/pages/api/profiles.ts
@@ -0,0 +1,23 @@
+import type { NextApiRequest, NextApiResponse } from "next"
+import { getProfiles } from "../../services/userService"
+import { UserProfile } from "../../lib/types"
+
+const GET = async (
+  req: NextApiRequest,
+  res: NextApiResponse<UserProfile[]>
+) => {
+  const profiles = await getProfiles()
+  res.send(profiles)
+}
+
+const handler = async (req: NextApiRequest, res: NextApiResponse) => {
+  switch (req.method) {
+    case "GET":
+      GET(req, res)
+      break
+    default:
+      res.status(405).end(`Method ${req.method} Not Allowed`)
+  }
+}
+
+export default handler
---
+++ src/pages/api/profiles/[id].ts
@@ -0,0 +1,30 @@
+import type { NextApiRequest, NextApiResponse } from "next"
+import { UserProfile } from "../../../lib/types"
+import { findProfileById } from "../../../services/userService"
+
+const GET = async (
+  req: NextApiRequest,
+  res: NextApiResponse<UserProfile>,
+  id: string
+) => {
+  const user = await findProfileById(id)
+  res.send(user!)
+}
+
+const handler = async (
+  req: NextApiRequest,
+  res: NextApiResponse<UserProfile | null>
+) => {
+  const { id } = req.query as { id: string }
+
+  switch (req.method) {
+    case "GET":
+      GET(req, res, id)
+      break
+
+    default:
+      res.status(405).end(`Method ${req.method} Not Allowed`)
+  }
+}
+
+export default handler
