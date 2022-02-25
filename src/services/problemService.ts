5f671aedd1c7471c3243a0fbc6ef9e157b4084a4
frozberry <93860959+frozberry@users.noreply.github.com>
2022-02-25 12:28:45 +0000

    create find function

---
+++ src/services/problemService.ts
@@ -0,0 +1,22 @@
+import { Problem } from "@prisma/client"
+import { prisma } from "../prisma/client"
+
+export const getProblems = async (): Promise<Problem[]> => {
+  const problems = await prisma.problem.findMany()
+  return problems
+}
+
+export const findProblemById = async (id: string): Promise<Problem | null> => {
+  const problem = await prisma.problem.findUnique({ where: { id } })
+  return problem
+}
+
+export const deleteProblem = async (id: string): Promise<boolean> => {
+  const problem = await findProblemById(id)
+  if (!problem) {
+    return false
+  }
+
+  await prisma.problem.delete({ where: { id } })
+  return true
+}
