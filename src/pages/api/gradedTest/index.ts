3285a8ac707a31b3b803dfd20897e2f1ba0ae7f8
frozberry <93860959+frozberry@users.noreply.github.com>
2022-02-25 19:53:44 +0000

    Create submit test route

---
+++ src/pages/api/gradedTest/index.ts
@@ -0,0 +1,57 @@
+import type { NextApiRequest, NextApiResponse } from "next"
+import { User } from "@prisma/client"
+
+import { createUser, editUser, getUsers } from "../../../services/userService"
+import verifyUser from "../../../lib/verifyUser"
+
+type PostBody = {
+  parentName: string
+  email: string
+  password: string
+}
+
+type PutBody = {
+  firstName: string
+  lastName: string
+  dob: string
+  gender: string
+}
+
+const GET = async (req: NextApiRequest, res: NextApiResponse<User[]>) => {
+  const users = await getUsers()
+  res.send(users)
+}
+
+const POST = async (req: NextApiRequest, res: NextApiResponse<User>) => {
+  const { parentName, email, password }: PostBody = req.body
+  const user = await createUser(parentName, email, password)
+  res.send(user!)
+}
+
+const PUT = async (req: NextApiRequest, res: NextApiResponse<User>) => {
+  const { firstName, lastName, dob, gender }: PutBody = req.body
+  const user = await verifyUser(req)
+  if (!user) {
+    return res.status(401).end()
+  }
+  const updatedUser = await editUser(user!, firstName, lastName, dob, gender)
+  res.send(updatedUser!)
+}
+
+const handler = async (req: NextApiRequest, res: NextApiResponse) => {
+  switch (req.method) {
+    case "GET":
+      GET(req, res)
+      break
+    case "POST":
+      POST(req, res)
+      break
+    case "PUT":
+      PUT(req, res)
+      break
+    default:
+      res.status(405).end(`Method ${req.method} Not Allowed`)
+  }
+}
+
+export default handler
---
+++ src/pages/api/gradedTest/submit.ts
@@ -0,0 +1,37 @@
+import type { NextApiRequest, NextApiResponse } from "next"
+import { GradedTest, User } from "@prisma/client"
+import verifyUser from "../../../lib/verifyUser"
+import { submitTest } from "../../../services/gradedTestService"
+
+// TODO use proper type
+type PostBody = {
+  testId: string
+  answers: any
+}
+
+const POST = async (
+  req: NextApiRequest,
+  res: NextApiResponse<GradedTest | null>
+) => {
+  const { testId, answers }: PostBody = req.body
+  const user = await verifyUser(req)
+
+  if (!user) {
+    return res.status(401).end("unauthoized token")
+  }
+
+  const gradedTest = await submitTest(user, testId, answers)
+  res.send(gradedTest)
+}
+
+const handler = async (req: NextApiRequest, res: NextApiResponse) => {
+  switch (req.method) {
+    case "POST":
+      POST(req, res)
+      break
+    default:
+      res.status(405).end(`Method ${req.method} Not Allowed`)
+  }
+}
+
+export default handler
