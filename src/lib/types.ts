403fb07f1fcd9ea19ed40d6f7509e94355f8a702
frozberry <93860959+frozberry@users.noreply.github.com>
2022-02-26 13:26:58 +0000

    Refactor types with type safety

--- src/lib/types.ts
+++ src/lib/types.ts
@@ -1,42 +1,57 @@
 import { Category, Prisma } from "@prisma/client"
 
-export type UserWithoutDate = Prisma.UserGetPayload<{
+/* ----------------------------- UserWithoutDate ---------------------------- */
+const userWithoutDate = Prisma.validator<Prisma.UserArgs>()({
   select: {
-    id: true
-    email: true
-    firstName: true
-    lastName: true
-    parentName: true
-    gender: true
-    admin: true
-    profilePicture: true
-    active: true
-    score: true
-    gradedExams: true
-    gradedCategories: true
-  }
-}>
+    id: true,
+    email: true,
+    firstName: true,
+    lastName: true,
+    parentName: true,
+    gender: true,
+    admin: true,
+    profilePicture: true,
+    active: true,
+    score: true,
+    gradedExams: true,
+    gradedCategories: true,
+  },
+})
 
-export type UserProfile = Prisma.UserGetPayload<{
+export type UserWithoutDate = Prisma.UserGetPayload<typeof userWithoutDate>
+
+/* ------------------------------- UserProfile ------------------------------ */
+const userProfile = Prisma.validator<Prisma.UserArgs>()({
   select: {
-    firstName: true
-    lastName: true
-    dob: true
-    gender: true
-    profilePicture: true
-    score: true
-  }
-}>
+    firstName: true,
+    lastName: true,
+    dob: true,
+    gender: true,
+    profilePicture: true,
+    score: true,
+  },
+})
 
-export type ProblemWithCategory = Prisma.ProblemGetPayload<{
-  include: { categories: true }
-}>
+export type UserProfile = Prisma.UserGetPayload<typeof userProfile>
 
+/* --------------------------- ProblemWithCategory -------------------------- */
+const problemWithCategory = Prisma.validator<Prisma.ProblemArgs>()({
+  include: {
+    categories: true,
+  },
+})
+
+export type ProblemWithCategory = Prisma.ProblemGetPayload<
+  typeof problemWithCategory
+>
+
+/* -------------------------------- Numbered -------------------------------- */
 // Generic type for Tests and GradedTests
 export type Numbered = {
   num: number
 }
 
+/* --------------------------- CategoryWithAverage -------------------------- */
 export type CategoryWithAverage = Category & {
   average: number
 }
