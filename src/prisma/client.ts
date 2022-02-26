984d063d520013b6ecab4373b60607c26547bb90
frozberry <93860959+frozberry@users.noreply.github.com>
2022-02-26 20:29:42 +0000

    Add prisma logging

--- src/prisma/client.ts
+++ src/prisma/client.ts
@@ -8,4 +8,15 @@ declare global {
 
 export const prisma = global.prisma || new PrismaClient()
 
+prisma.$use(async (params, next) => {
+  const before = Date.now()
+  const result = await next(params)
+  const after = Date.now()
+  console.log(
+    "\x1b[32m",
+    `Query ${params.model}.${params.action} took ${after - before}ms`
+  )
+  return result
+})
+
 if (process.env.NODE_ENV !== "production") global.prisma = prisma
