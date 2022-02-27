bcf1c6f53795db34d429c3a195e4190efd0711fc
frozberry <93860959+frozberry@users.noreply.github.com>
2022-02-27 19:06:48 +0000

    Create basic results page

--- src/pages/results/index.tsx
+++ src/pages/results/index.tsx
@@ -1,11 +1,72 @@
-import { Container, Typography } from "@mui/material"
+import { Container, Typography, Divider } from "@mui/material"
+import Link from "next/link"
+import { useQuery } from "react-query"
+import dayjs from "dayjs"
+import axios from "axios"
 
-const Page = () => {
+const styles = {
+  divider: {
+    marginTop: 20,
+    marginBottom: 20,
+  },
+}
+
+const ResultsList = () => {
+  const { isLoading, error, data } = useQuery("gradedTestsData", () =>
+    axios.get("/api/graded-exams/all").then((res) => res.data)
+  )
+
+  if (isLoading) return "Loading query..."
+
+  console.log(data)
+
   return (
     <Container>
-      <Typography>Hello</Typography>
+      {data.map((gradedTest: any) => (
+        <SingleAttempt key={gradedTest.id} gt={gradedTest} />
+      ))}
     </Container>
   )
 }
 
-export default Page
+const SingleAttempt = ({ gt }: any) => {
+  return (
+    <Link href={`/results/${gt.id}`} passHref>
+      <div>
+        <Typography variant="h4">Maths Test {gt.num}</Typography>
+        <Typography>{dayjs(gt.date).format("D MMM h:mma")}</Typography>
+        <Typography>{gt.percent}%</Typography>
+        <Typography>
+          {gt.marks}/{gt.total} marks
+        </Typography>
+        <Divider style={styles.divider} />
+      </div>
+    </Link>
+  )
+}
+
+// const MultipleAttempts = ({ test }) => {
+//   const profile = useSelector((state) => state.profile)
+//   const attempts = test.attempts.length
+//   const averagePercent = Math.round(_.meanBy(test.attempts, (a) => a.percent))
+//   const averageMarks = Math.round(_.meanBy(test.attempts, (a) => a.marks))
+//   const totalMarks = test.attempts[0].total
+//   return (
+//     <Link
+//       href={`/results/test/${test.testId}`}
+//       style={{ textDecoration: "none", color: "black", cursor: "pointer" }}
+//     >
+//       <Typography variant="h4">Maths Test {test.attempts[0].num}</Typography>
+//       <Typography>
+//         {profile?.firstName} has attempted this test {attempts} times
+//       </Typography>
+//       <Typography>{averagePercent}%</Typography>
+//       <Typography>
+//         {averageMarks}/{totalMarks} marks
+//       </Typography>
+//       <Divider style={styles.divider} />
+//     </Link>
+//   )
+// }
+
+export default ResultsList
