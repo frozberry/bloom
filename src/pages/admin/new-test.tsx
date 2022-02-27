741ee22d1d8604d3d52086de3d2002b8c31cf75b
frozberry <93860959+frozberry@users.noreply.github.com>
2022-02-27 14:25:58 +0000

    Add pages structure

---
+++ src/pages/admin/new-test.tsx
@@ -0,0 +1,12 @@
+
+import { Container, Typography } from "@mui/material"
+
+const Page = () => {
+  return (
+    <Container>
+      <Typography>Hello</Typography>
+    </Container>
+  )
+}
+
+export default Page
---
+++ src/pages/exam/index.tsx
@@ -0,0 +1,11 @@
+import { Container, Typography } from "@mui/material"
+
+const Page = () => {
+  return (
+    <Container>
+      <Typography>Exam</Typography>
+    </Container>
+  )
+}
+
+export default Page
