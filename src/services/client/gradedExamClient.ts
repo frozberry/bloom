9178007c77ec3d2d8bdfadb26de99d26d4c38acf
frozberry <93860959+frozberry@users.noreply.github.com>
2022-02-28 08:56:38 +0000

    Refactor setting auth config

--- src/services/client/gradedExamClient.ts
+++ src/services/client/gradedExamClient.ts
@@ -1,21 +1,10 @@
 import axios from "axios"
+import setAuthToken from "../../lib/setAuthToken"
 
 const url = "/api/graded-exams"
 
 export const getSortedTests = async () => {
-  console.log("get sorted tests")
-
-  let user
-  const loggedUserJson = localStorage.getItem("loggedWaterfrontUser")
-  if (loggedUserJson) {
-    user = JSON.parse(loggedUserJson)
-  }
-
-  const config = {
-    headers: {
-      Authorization: `Bearer ${user.token}`,
-    },
-  }
+  const config = setAuthToken()
 
   const response = await axios.get(`${url}/test-sorted`, config)
   return response.data
