106557e7c7ba9496e9e9a14a3db99428e3a800fd
frozberry <93860959+frozberry@users.noreply.github.com>
2022-03-02 14:38:16 +0000

    Fix user types

--- src/pages/api/users/index.ts
+++ src/pages/api/users/index.ts
@@ -7,6 +7,7 @@ import {
   getUsers,
 } from "../../../services/server/userService"
 import verifyUser from "../../../lib/verifyUser"
+import { UserWithoutDate } from "../../../lib/types"
 
 type PostBody = {
   parentName: string
@@ -21,22 +22,25 @@ type PutBody = {
   gender: string
 }
 
-const GET = async (req: NextApiRequest, res: NextApiResponse) => {
+const GET = async (
+  req: NextApiRequest,
+  res: NextApiResponse<UserWithoutDate[]>
+) => {
   const users = await getUsers()
   res.send(users)
 }
 
-const POST = async (req: NextApiRequest, res: NextApiResponse<User | null>) => {
+const POST = async (req: NextApiRequest, res: NextApiResponse<string>) => {
   const { parentName, email, password }: PostBody = req.body
   const user = await createUser(parentName, email, password)
   res.send(user)
 }
 
-const PUT = async (req: NextApiRequest, res: NextApiResponse<User | null>) => {
+const PUT = async (req: NextApiRequest, res: NextApiResponse<User>) => {
   const { firstName, lastName, dob, gender }: PutBody = req.body
   const user = await verifyUser(req)
   if (!user) {
-    return res.status(401).end()
+    return res.status(401).end("unauthorized")
   }
   const updatedUser = await editUser(user, firstName, lastName, dob, gender)
   res.send(updatedUser)
