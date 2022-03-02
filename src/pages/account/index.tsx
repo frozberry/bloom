99f00640b35f428eda035b32cf79f6eafd13c9a7
frozberry <93860959+frozberry@users.noreply.github.com>
2022-03-02 20:43:58 +0000

    Fix button

--- src/pages/account/index.tsx
+++ src/pages/account/index.tsx
@@ -76,7 +76,6 @@ export default function App() {
               color="primary"
               fullWidth
               disabled={formikProps.isSubmitting}
-              align="center"
             >
               Save
             </Button>
