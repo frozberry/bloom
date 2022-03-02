86b5ca0b65578a0a875b3fd819e9eda9162b2cde
frozberry <93860959+frozberry@users.noreply.github.com>
2022-03-02 16:33:27 +0000

    Move error recast

--- src/lib/notifyError.ts
+++ src/lib/notifyError.ts
@@ -5,6 +5,7 @@ import { ServerError } from "./types"
 const notifyError = (e: any) => {
   const error = e as AxiosError<ServerError>
   if (axios.isAxiosError(error)) {
+    const error = e as AxiosError<ServerError>
     toast.error(error.response?.data.message as string)
   } else {
     toast.error("Unexpected error")
