4ae52ad8f7ab17120034cfd68970db0ae579a684
frozberry <93860959+frozberry@users.noreply.github.com>
2022-03-03 18:02:21 +0000

    Update schema

--- src/prisma/migrations/20220303175959_init/migration.sql
+++ src/prisma/migrations/20220303175959_init/migration.sql
@@ -4,13 +4,16 @@ CREATE TYPE "Category" AS ENUM ('NUMBERS', 'ADDITION_SUBTRACTION', 'MULTIPLICATI
 -- CreateTable
 CREATE TABLE "User" (
     "id" TEXT NOT NULL,
+    "name" TEXT,
     "email" TEXT NOT NULL,
+    "emailVerified" TIMESTAMP(3),
+    "image" TEXT,
     "firstName" TEXT,
     "lastName" TEXT,
-    "parentName" TEXT NOT NULL,
+    "parentName" TEXT,
+    "passwordHash" TEXT,
     "dob" TIMESTAMP(3),
     "gender" TEXT,
-    "passwordHash" TEXT NOT NULL,
     "admin" BOOLEAN NOT NULL DEFAULT false,
     "profilePicture" TEXT,
     "stripeId" TEXT,
@@ -105,6 +108,42 @@ CREATE TABLE "GradedCategory" (
     CONSTRAINT "GradedCategory_pkey" PRIMARY KEY ("id")
 );
 
+-- CreateTable
+CREATE TABLE "Account" (
+    "id" TEXT NOT NULL,
+    "userId" TEXT NOT NULL,
+    "type" TEXT NOT NULL,
+    "provider" TEXT NOT NULL,
+    "providerAccountId" TEXT NOT NULL,
+    "refresh_token" TEXT,
+    "refresh_token_expires_in" INTEGER,
+    "access_token" TEXT,
+    "expires_at" INTEGER,
+    "token_type" TEXT,
+    "scope" TEXT,
+    "id_token" TEXT,
+    "session_state" TEXT,
+
+    CONSTRAINT "Account_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Session" (
+    "id" TEXT NOT NULL,
+    "sessionToken" TEXT NOT NULL,
+    "userId" TEXT NOT NULL,
+    "expires" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "Session_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "VerificationToken" (
+    "identifier" TEXT NOT NULL,
+    "token" TEXT NOT NULL,
+    "expires" TIMESTAMP(3) NOT NULL
+);
+
 -- CreateIndex
 CREATE UNIQUE INDEX "User_email_key" ON "User"("email");
 
@@ -116,6 +155,18 @@ CREATE UNIQUE INDEX "ExamSession_userId_key" ON "ExamSession"("userId");
 
 -- CreateIndex
 CREATE UNIQUE INDEX "GradedCategory_userId_category_key" ON "GradedCategory"("userId", "category");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Account_provider_providerAccountId_key" ON "Account"("provider", "providerAccountId");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Session_sessionToken_key" ON "Session"("sessionToken");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "VerificationToken_token_key" ON "VerificationToken"("token");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "VerificationToken_identifier_token_key" ON "VerificationToken"("identifier", "token");
 
 -- AddForeignKey
 ALTER TABLE "Problem" ADD CONSTRAINT "Problem_examId_fkey" FOREIGN KEY ("examId") REFERENCES "Exam"("id") ON DELETE CASCADE ON UPDATE CASCADE;
@@ -137,3 +188,9 @@ ALTER TABLE "ExamSession" ADD CONSTRAINT "ExamSession_examId_fkey" FOREIGN KEY (
 
 -- AddForeignKey
 ALTER TABLE "GradedCategory" ADD CONSTRAINT "GradedCategory_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Account" ADD CONSTRAINT "Account_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Session" ADD CONSTRAINT "Session_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE CASCADE ON UPDATE CASCADE;
