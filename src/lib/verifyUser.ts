01d39b9d9712988d9c48ae0e74ad76240f160f7f
frozberry <93860959+frozberry@users.noreply.github.com>
2022-03-03 13:56:04 +0000

    Remove old code

--- src/lib/verifyUser.ts
+++ src/lib/verifyUser.ts
@@ -1,41 +1,17 @@
 import { User } from "@prisma/client"
-import jwt from "jsonwebtoken"
 import { NextApiRequest } from "next"
 import { getSession } from "next-auth/react"
-import { prisma } from "../prisma/client"
-
-type Token = {
-  id: string
-  email: string
-}
+import { findUserById } from "../services/server/userService"
 
 const verifyUser = async (req: NextApiRequest): Promise<User | null> => {
-  // const { authorization } = req.headers
-  // if (!authorization) {
-  //   return null
-  // }
-
-  // const token = authorization.substring(7)
-
-  // // eslint-disable-next-line
-  // const decodedToken = jwt.verify(token, process.env.JWT_SECRET!) as Token
-
-  // if (!decodedToken) {
-  //   return null
-  // }
-
-  // const user = await prisma.user.findUnique({
-  //   where: {
-  //     id: decodedToken.id,
-  //   },
-  // })
+  // TODO maybe can type the session properly
   const session = await getSession({ req })
 
-  const user = await prisma.user.findUnique({
-    where: {
-      email: session.user.email as string,
-    },
-  })
+  if (!session) {
+    return null
+  }
+
+  const user = await findUserById(session.id as string)
 
   return user
 }
