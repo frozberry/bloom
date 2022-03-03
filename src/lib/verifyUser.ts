287bfc1a01de74876ffaabea69a7a774c8599f3a
frozberry <93860959+frozberry@users.noreply.github.com>
2022-03-03 13:49:12 +0000

    Use session in api route

--- src/lib/verifyUser.ts
+++ src/lib/verifyUser.ts
@@ -1,6 +1,7 @@
 import { User } from "@prisma/client"
 import jwt from "jsonwebtoken"
 import { NextApiRequest } from "next"
+import { getSession } from "next-auth/react"
 import { prisma } from "../prisma/client"
 
 type Token = {
@@ -9,23 +10,30 @@ type Token = {
 }
 
 const verifyUser = async (req: NextApiRequest): Promise<User | null> => {
-  const { authorization } = req.headers
-  if (!authorization) {
-    return null
-  }
+  // const { authorization } = req.headers
+  // if (!authorization) {
+  //   return null
+  // }
 
-  const token = authorization.substring(7)
+  // const token = authorization.substring(7)
 
-  // eslint-disable-next-line
-  const decodedToken = jwt.verify(token, process.env.JWT_SECRET!) as Token
+  // // eslint-disable-next-line
+  // const decodedToken = jwt.verify(token, process.env.JWT_SECRET!) as Token
 
-  if (!decodedToken) {
-    return null
-  }
+  // if (!decodedToken) {
+  //   return null
+  // }
+
+  // const user = await prisma.user.findUnique({
+  //   where: {
+  //     id: decodedToken.id,
+  //   },
+  // })
+  const session = await getSession({ req })
 
   const user = await prisma.user.findUnique({
     where: {
-      id: decodedToken.id,
+      email: session.user.email as string,
     },
   })
 
