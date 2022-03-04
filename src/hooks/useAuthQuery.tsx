3b5561167e61ef152c0f434862090088be89edda
frozberry <93860959+frozberry@users.noreply.github.com>
2022-03-04 20:55:42 +0000

    Remove session from authQuery hook

--- src/hooks/useAuthQuery.tsx
+++ src/hooks/useAuthQuery.tsx
@@ -1,11 +1,9 @@
-import { useSession } from "next-auth/react"
 import { QueryFunction, useQuery } from "react-query"
 
 const useAuthQuery = (key: string, queryFn: QueryFunction) => {
   const { isLoading, error, data } = useQuery(key, queryFn)
-  const { data: session } = useSession()
 
-  return { session, isLoading, error, data }
+  return { isLoading, error, data }
 }
 
 export default useAuthQuery
