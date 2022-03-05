33286f616ec82ab50160770de78ea6c8a42124bb
frozberry <93860959+frozberry@users.noreply.github.com>
2022-03-05 12:44:35 +0000

    Add prisma fixes

---
+++ src/prisma/migrations/20220305124418_fix2/migration.sql
@@ -0,0 +1,8 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `num` on the `GradedExam` table. All the data in the column will be lost.
+
+*/
+-- AlterTable
+ALTER TABLE "GradedExam" DROP COLUMN "num";
