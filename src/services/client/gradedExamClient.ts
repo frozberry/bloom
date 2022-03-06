2552f41dd1949260516d4eea0fe733667688048c
frozberry <93860959+frozberry@users.noreply.github.com>
2022-03-06 08:43:29 +0000

    Create submit exam client

--- src/services/client/gradedExamClient.ts
+++ src/services/client/gradedExamClient.ts
@@ -1,6 +1,6 @@
 import { GradedExam } from "@prisma/client"
 import axios from "axios"
-import { ExamResultOverivew } from "../../lib/types"
+import { ExamResultOverivew, ProblemSubmission } from "../../lib/types"
 
 const url = "/api/graded-exams"
 
@@ -16,5 +16,14 @@ export const findGradedExamById = async (id: string) => {
 
 export const getExamResultsOverview = async () => {
   const res = await axios.get<ExamResultOverivew[]>(`${url}/overview`)
+  return res.data
+}
+
+export const submitExam = async (
+  examSessionId: string,
+  submissions: ProblemSubmission[]
+) => {
+  const data = { examSessionId, submissions }
+  const res = await axios.post("url", data)
   return res.data
 }
