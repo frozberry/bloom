676088161e70e65835f244294beed10b3a422df2
frozberry <93860959+frozberry@users.noreply.github.com>
2022-03-06 08:41:16 +0000

    Move submit route to index

--- src/pages/api/graded-exams/index.ts
+++ src/pages/api/graded-exams/index.ts
@@ -1,7 +1,16 @@
 import { GradedExam } from "@prisma/client"
 import type { NextApiRequest, NextApiResponse } from "next"
 import authUserSession from "../../../lib/authUserSession"
-import { getUsersGradedExams } from "../../../services/server/gradedExamService"
+import { ProblemSubmission } from "../../../lib/types"
+import {
+  getUsersGradedExams,
+  submitExam,
+} from "../../../services/server/gradedExamService"
+
+type PostBody = {
+  examId: string
+  submissions: ProblemSubmission[]
+}
 
 const GET = async (req: NextApiRequest, res: NextApiResponse<GradedExam[]>) => {
   const { unauthorized, userId, response } = await authUserSession(req, res)
@@ -11,10 +20,26 @@ const GET = async (req: NextApiRequest, res: NextApiResponse<GradedExam[]>) => {
   res.send(gradedExams)
 }
 
+const POST = async (
+  req: NextApiRequest,
+  res: NextApiResponse<GradedExam | null>
+) => {
+  const { examId, submissions }: PostBody = req.body
+
+  const { unauthorized, userId, response } = await authUserSession(req, res)
+  if (unauthorized) return response
+
+  const gradedExam = await submitExam(userId, examId, submissions)
+  res.send(gradedExam)
+}
+
 const handler = async (req: NextApiRequest, res: NextApiResponse) => {
   switch (req.method) {
     case "GET":
       GET(req, res)
+      break
+    case "POST":
+      POST(req, res)
       break
     default:
       res.status(405).end(`Method ${req.method} Not Allowed`)
