e8db6f35e55900c46592efd4a3a900f16088615f
frozberry <93860959+frozberry@users.noreply.github.com>
2022-03-08 11:56:12 +0000

    Create loading and use notify error

--- src/pages/results/graded-exams/[id].tsx
+++ src/pages/results/graded-exams/[id].tsx
@@ -1,13 +1,22 @@
-import { Button, Container, Paper, Typography } from "@mui/material"
+import {
+  Button,
+  Container,
+  LinearProgress,
+  Paper,
+  Typography,
+} from "@mui/material"
 import { useRouter } from "next/router"
+import { useState } from "react"
 import Answers from "../../../components/results/Answers"
 import useAuthQuery from "../../../hooks/useAuthQuery"
+import notifyError from "../../../lib/notifyError"
 import { GradedExamWithGradedProblems } from "../../../lib/types"
 import { createExamSession } from "../../../services/client/examSessionClient"
 import { findGradedExamById } from "../../../services/client/gradedExamClient"
 
 const Page = () => {
   const router = useRouter()
+  const [loading, setLoading] = useState(false)
   const { id } = router.query as { id: string }
   const { data, escape, component } = useAuthQuery(id, () =>
     findGradedExamById(id)
@@ -23,39 +32,45 @@ const Page = () => {
       )
     ) {
       try {
+        setLoading(true)
         await createExamSession(gradedExam.examId)
+        setLoading(false)
         router.push("/session")
-      } catch {
-        router.push("/session")
+      } catch (e) {
+        notifyError(e)
+        setLoading(false)
       }
     }
   }
 
   return (
-    <Container
-      sx={{
-        display: "flex",
-        flexDirection: "column",
-        alignItems: "center",
-        marginTop: 3,
-      }}
-    >
-      <Paper sx={{ px: 10, py: 4 }}>
-        <Typography align="center" variant="h6">
-          You scored
-        </Typography>
-        <Typography align="center" variant="h3">
-          {Math.round(gradedExam.percent)}%
-        </Typography>
-        <Typography align="center">
-          {`${gradedExam.marks}/${gradedExam.totalMarks}`} marks
-        </Typography>
-        <Button variant="contained" onClick={retryTest} sx={{ mt: 2 }}>
-          Retry test
-        </Button>
-      </Paper>
-      <Answers gradedProblems={gradedExam.gradedProblems} />
-    </Container>
+    <>
+      {loading && <LinearProgress />}
+      <Container
+        sx={{
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "center",
+          marginTop: 3,
+        }}
+      >
+        <Paper sx={{ px: 10, py: 4 }}>
+          <Typography align="center" variant="h6">
+            You scored
+          </Typography>
+          <Typography align="center" variant="h3">
+            {Math.round(gradedExam.percent)}%
+          </Typography>
+          <Typography align="center">
+            {`${gradedExam.marks}/${gradedExam.totalMarks}`} marks
+          </Typography>
+          <Button variant="contained" onClick={retryTest} sx={{ mt: 2 }}>
+            Retry test
+          </Button>
+        </Paper>
+        <Answers gradedProblems={gradedExam.gradedProblems} />
+      </Container>
+    </>
   )
 }
 
