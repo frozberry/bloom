5d938ab483cd4de28aa288c22f70952ac4458a7b
frozberry <93860959+frozberry@users.noreply.github.com>
2022-03-08 09:17:42 +0000

    Add customer portal

--- src/pages/account.tsx
+++ src/pages/account.tsx
@@ -1,10 +1,11 @@
-import { Box, Container, Divider, Typography } from "@mui/material"
+import { Box, Button, Container, Divider, Typography } from "@mui/material"
 import ChangePasswordForm from "../components/forms/ChangePasswordForm"
 import ChildForm from "../components/forms/ChildForm"
 import useAuthQuery from "../hooks/useAuthQuery"
 import { UserProfile } from "../lib/types"
 import { isUserOAuth } from "../services/client/accountClient"
 import { findUsersProfile } from "../services/client/profileClient"
+import { stripePortal } from "../services/client/stripeClient"
 
 type Data = {
   profile: UserProfile
@@ -15,6 +16,11 @@ const getAccountInfo = async (): Promise<Data> => {
   const profile = await findUsersProfile()
   const isOAuth = await isUserOAuth()
   return { profile, isOAuth }
+}
+
+const openPortal = async () => {
+  const url = await stripePortal("cus_JifnHyNvFpbJIx")
+  window.open(url)
 }
 
 export default function App() {
@@ -41,6 +47,22 @@ export default function App() {
           <ChangePasswordForm />
         </>
       )}
+      <Divider sx={{ my: 4 }} />
+      <Typography variant="h2" sx={{ my: 2 }}>
+        Membership and billing
+      </Typography>
+      <Typography sx={{ textAlign: "center" }}>
+        Click here to access our secure billing portal
+      </Typography>
+      <Button
+        variant="contained"
+        size="large"
+        color="secondary"
+        fullWidth
+        onClick={openPortal}
+      >
+        Payments portal
+      </Button>
     </Container>
   )
 }
