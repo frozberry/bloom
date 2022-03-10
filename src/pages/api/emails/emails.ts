07ed81246ba2a6ff6ce05f10d2e09c9ecec5b3ad
frozberry <93860959+frozberry@users.noreply.github.com>
2022-03-10 09:15:04 +0000

    Use named export across api

---
+++ src/pages/api/emails/emails.ts
@@ -0,0 +1,32 @@
+import type { NextApiRequest, NextApiResponse } from "next"
+import { sendPasswordResetEmail } from "../../../services/server/emailService"
+import { findUserByEmail } from "../../../services/server/userService"
+
+type PostBody = {
+  email: string
+}
+
+const POST = async (req: NextApiRequest, res: NextApiResponse) => {
+  const { email }: PostBody = req.body
+  const user = await findUserByEmail(email)
+
+  if (!user) {
+    return res.status(401).end("email not found")
+  }
+
+  sendPasswordResetEmail(user.id, user.email)
+
+  res.status(200).end(`email sent to ${user.email}`)
+}
+
+const handler = async (req: NextApiRequest, res: NextApiResponse) => {
+  switch (req.method) {
+    case "POST":
+      POST(req, res)
+      break
+    default:
+      res.status(405).end(`Method ${req.method} Not Allowed`)
+  }
+}
+
+export default handler
