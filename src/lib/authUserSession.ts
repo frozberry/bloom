369f55b52f6509ee083b32fa514ba4af91556200
frozberry <93860959+frozberry@users.noreply.github.com>
2022-03-10 19:21:08 +0000

    Check for active subscription

--- src/lib/authUserSession.ts
+++ src/lib/authUserSession.ts
@@ -1,19 +1,32 @@
 import { getSession } from "next-auth/react"
 import { NextApiRequest, NextApiResponse } from "next/types"
+import { findUserById } from "../services/server/userService"
 
-const authUserSession = async (
-  req: NextApiRequest,
-  res: NextApiResponse
-) => {
+const authUserSession = async (req: NextApiRequest, res: NextApiResponse) => {
   const session = await getSession({ req })
-  if (session) {
-    return { unauthorized: false, userId: session.id as string }
+
+  if (!session) {
+    return {
+      unauthorized: true,
+      response: res.status(401).end("You must be logged in to do that"),
+      userId: "",
+    }
   }
-  return {
-    unauthorized: true,
-    response: res.status(401).end("unauthorized"),
-    userId: "",
+
+  const user = await findUserById(session.id as string)
+
+  if (!user?.active) {
+    return {
+      unauthorized: true,
+      response: res.status(401).send({
+        type: "inactiveSubscription",
+        message: "Subscription is inactive",
+      }),
+      userId: "",
+    }
   }
+
+  return { unauthorized: false, userId: session.id as string }
 }
 
 export default authUserSession
