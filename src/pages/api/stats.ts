b52ef013ead2cbad4f87e2c44adef9fd7a668750
frozberry <93860959+frozberry@users.noreply.github.com>
2022-03-12 23:20:05 +0000

    Create single route for stats page

--- src/pages/api/stats.ts
+++ src/pages/api/stats.ts
@@ -1,18 +1,32 @@
 import { User } from "@prisma/client"
 import type { NextApiRequest, NextApiResponse } from "next"
 import authUserSession from "../../lib/authUserSession"
+import { CategoryStatsData } from "../../lib/types"
+import { getUsersGradedCategoriesWithAverage } from "../../services/server/gradedCategoryService"
 import { findUserById } from "../../services/server/userService"
+
+export type StatsPageData = {
+  categoriesData: CategoryStatsData
+  score: number | null
+}
 
 // TODO this route is a quick hack
 const GET = async (
   req: NextApiRequest,
-  res: NextApiResponse<{ score: number | null }>
+  res: NextApiResponse<StatsPageData>
 ) => {
   const { unauthorized, userId, response } = await authUserSession(req, res)
   if (unauthorized) return response
 
+  const averages = await getUsersGradedCategoriesWithAverage(userId)
   const user = (await findUserById(userId)) as User
-  res.send({ score: user.score })
+
+  const data = {
+    categoriesData: averages,
+    score: user.score,
+  }
+
+  res.send(data)
 }
 
 const handler = async (req: NextApiRequest, res: NextApiResponse) => {
