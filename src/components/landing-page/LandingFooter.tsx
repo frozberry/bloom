2b574a245fe9aa5d78b6f80dabe70985b48e5197
frozberry <93860959+frozberry@users.noreply.github.com>
2022-04-10 16:09:46 +0100

    Format landing header

--- src/components/landing-page/LandingFooter.tsx
+++ src/components/landing-page/LandingFooter.tsx
@@ -9,36 +9,48 @@ const LandingFooter = () => {
       maxWidth={false}
       sx={{
         backgroundColor: "lightBlue.main",
-        py: 4,
-        px: 6,
+        py: {
+          xs: 1,
+          sm: 4,
+        },
       }}
     >
-      <Container maxWidth="md">
+      <Container>
         <Typography sx={{ textAlign: "center", my: 5 }} variant="h3">
           <b>Start your child's journey to a top grammar school today</b>
         </Typography>
-        <Typography
-          variant="h5"
-          sx={{
-            letterSpacing: 1,
-            textAlign: "center",
-            marginBottom: 10,
-          }}
-        >
-          Try Waterfront free for 14-days
-        </Typography>
+
         <Link href="/signup" passHref>
           <Button
             variant="contained"
             color="primary"
             fullWidth
-            // onClick={() => history.push("/signup")}
-            sx={{ mb: 5, py: 3, fontSize: 20, textTransform: "none" }}
+            sx={{
+              mb: 5,
+              py: {
+                xs: 2,
+                sm: 3,
+              },
+              fontSize: {
+                xs: 16,
+                sm: 20,
+              },
+              textTransform: "none",
+            }}
           >
             Start preparing for the 11+ exam
           </Button>
         </Link>
-        <Typography sx={{ textAlign: "center" }}>
+
+        <Typography
+          sx={{
+            textAlign: "center",
+            fontSize: {
+              xs: 16,
+              sm: 20,
+            },
+          }}
+        >
           Get started today with zero risk
         </Typography>
       </Container>
