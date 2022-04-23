f4532f22caa71e1d849200a516c496557c2ff246
frozberry <93860959+frozberry@users.noreply.github.com>
2022-04-23 15:08:36 +0100

    Pricing page

---
+++ src/pages/pricing.tsx
@@ -0,0 +1,65 @@
+import { Box, Container, Typography } from "@mui/material"
+import Bullets from "../components/select-plan/Bullets"
+import PlanOption from "../components/select-plan/PlanOption"
+import Questions from "../components/select-plan/SubscriptionQuestions"
+
+const SelectPlan = () => {
+  return (
+    <Container sx={{ mt: 4 }}>
+      <Typography
+        variant="h4"
+        color="textPrimary"
+        sx={{ textAlign: "center", mb: 1 }}
+      >
+        {/* <b>The best investment you will make for you child's uture</b>
+        <b>Invest in your child's future with a Bloom subscription</b>
+        <b>Bloom is the best investment in your child's future</b>
+        <b>
+          A Bloom subscription is the best investment in your child's future
+        </b>
+        <b>Bloom is the smartest investment in your child's future</b>
+        <b>The best investment in your child's future you'll ever make</b> */}
+        <b>Invest in your child's future today</b>
+      </Typography>
+      <Typography style={{ textAlign: "center", fontSize: 18 }}>
+        All plans come with a <b>14-day free trial</b>
+      </Typography>
+      <Box
+        sx={{
+          display: "flex",
+          flexDirection: {
+            xs: "column",
+            sm: "row",
+          },
+          justifyContent: "center",
+          alignItems: "center",
+        }}
+      >
+        <PlanOption
+          title="Monthly"
+          price="£19.99 / month"
+          // onClick={() => handleCheckout("month")}
+        />
+        <PlanOption
+          title="Annual (2 months free)"
+          price="£16.99 / month"
+          // onClick={() => handleCheckout("year")}
+        />
+      </Box>
+
+      <Container
+        sx={{
+          display: "flex",
+          flexDirection: { xs: "column", sm: "row" },
+          mt: { xs: 2, sm: 5 },
+        }}
+        maxWidth="md"
+      >
+        <Questions />
+        <Bullets />
+      </Container>
+    </Container>
+  )
+}
+
+export default SelectPlan
