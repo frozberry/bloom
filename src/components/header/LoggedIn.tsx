0657b8db14871fd743911d05ef4b5fcd6647719b
frozberry <93860959+frozberry@users.noreply.github.com>
2022-04-23 16:14:55 +0100

    Refactor web and full size to components

--- src/components/header/LoggedIn.tsx
+++ src/components/header/LoggedIn.tsx
@@ -1,10 +1,25 @@
 import MenuIcon from "@mui/icons-material/Menu"
-import { Box, Button, Menu, MenuItem } from "@mui/material"
+import { Box, Menu, MenuItem } from "@mui/material"
 import { signOut } from "next-auth/react"
 import { useState } from "react"
 import { HeaderButton, HeaderLink } from "./HeaderComponents"
 
-const LoggedIn = () => {
+type Props = {
+  handleLogout: () => void
+}
+
+const FullSize = ({ handleLogout }: Props) => {
+  return (
+    <Box sx={{ display: { xs: "none", sm: "initial" } }}>
+      <HeaderLink link="/results">Results</HeaderLink>
+      <HeaderLink link="/stats">Stats</HeaderLink>
+      <HeaderLink link="/account">Account</HeaderLink>
+      <HeaderButton onClick={handleLogout}>Log out</HeaderButton>
+    </Box>
+  )
+}
+
+const Mobile = ({ handleLogout }: Props) => {
   const [anchorEl, setAnchorEl] = useState(null)
   const open = Boolean(anchorEl)
 
@@ -16,55 +31,52 @@ const LoggedIn = () => {
     setAnchorEl(null)
   }
 
+  return (
+    <Box sx={{ display: { xs: "initial", sm: "none" } }}>
+      <MenuIcon
+        fontSize="large"
+        color="primary"
+        sx={{ color: "black" }}
+        id="basic-button"
+        aria-controls={open ? "basic-menu" : undefined}
+        aria-haspopup="true"
+        aria-expanded={open ? "true" : undefined}
+        onClick={handleMenu}
+      />
+      <Menu
+        id="basic-menu"
+        anchorEl={anchorEl}
+        open={open}
+        onClose={handleClose}
+        MenuListProps={{
+          "aria-labelledby": "basic-button",
+        }}
+      >
+        <MenuItem onClick={handleClose}>
+          <HeaderLink link="/results">Results</HeaderLink>
+        </MenuItem>
+        <MenuItem onClick={handleClose}>
+          <HeaderLink link="/stats">Stats</HeaderLink>
+        </MenuItem>
+        <MenuItem onClick={handleClose}>
+          <HeaderLink link="/account">Account</HeaderLink>
+        </MenuItem>
+        <MenuItem onClick={handleClose}>
+          <HeaderButton onClick={handleLogout}>Log out</HeaderButton>
+        </MenuItem>
+      </Menu>
+    </Box>
+  )
+}
+
+const LoggedIn = () => {
   const handleLogout = () => {
     signOut({ callbackUrl: `${process.env.NEXT_PUBLIC_URL}/login` })
   }
-
   return (
     <Box>
-      <Box sx={{ display: { xs: "none", sm: "initial" } }}>
-        <HeaderLink link="/results">Results</HeaderLink>
-        <HeaderLink link="/stats">Stats</HeaderLink>
-        <HeaderLink link="/account">Account</HeaderLink>
-        <HeaderButton onClick={handleLogout}>Log out</HeaderButton>
-      </Box>
-
-      <Box sx={{ display: { xs: "initial", sm: "none" } }}>
-        <MenuIcon
-          fontSize="large"
-          color="primary"
-          sx={{ color: "black" }}
-          id="basic-button"
-          aria-controls={open ? "basic-menu" : undefined}
-          aria-haspopup="true"
-          aria-expanded={open ? "true" : undefined}
-          onClick={handleMenu}
-        />
-        <Menu
-          id="basic-menu"
-          anchorEl={anchorEl}
-          open={open}
-          onClose={handleClose}
-          MenuListProps={{
-            "aria-labelledby": "basic-button",
-          }}
-        >
-          <MenuItem onClick={handleClose}>
-            <HeaderLink link="/results">Results</HeaderLink>
-          </MenuItem>
-          <MenuItem onClick={handleClose}>
-            <HeaderLink link="/stats">Stats</HeaderLink>
-          </MenuItem>
-          <MenuItem onClick={handleClose}>
-            <HeaderLink link="/account">Account</HeaderLink>
-          </MenuItem>
-          <MenuItem onClick={handleClose}>
-            <Button sx={{ color: "black" }} onClick={handleLogout}>
-              Log out
-            </Button>
-          </MenuItem>
-        </Menu>
-      </Box>
+      <FullSize handleLogout={handleLogout} />
+      <Mobile handleLogout={handleLogout} />
     </Box>
   )
 }
