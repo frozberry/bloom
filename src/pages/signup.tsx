0c382c09e2543d74ba9a6f2bd5a0d2254a861b39
frozberry <93860959+frozberry@users.noreply.github.com>
2022-04-23 16:23:56 +0100

    Remove import

--- src/pages/signup.tsx
+++ src/pages/signup.tsx
@@ -3,9 +3,8 @@ import {
   Container,
   Divider,
   LinearProgress,
-  Typography,
+  Typography
 } from "@mui/material"
-import Image from "next/image"
 import Link from "next/link"
 import { useState } from "react"
 import SignupForm from "../components/forms/SignupForm"
